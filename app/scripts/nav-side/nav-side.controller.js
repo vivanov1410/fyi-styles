7e8952004b5cbdb1243c1cfe490f11e80f497944
Svetlana Ivanova <sveta.Ivanova@fyidoctors.com>
2014-09-27 16:32:18 -0700

    added nav-side controller

--- app/scripts/nav-side/nav-side.controller.js
+++ app/scripts/nav-side/nav-side.controller.js
@@ -1,6 +1,58 @@
 'use strict';
 
 angular.module('fyi-styles')
-.controller('NavSideCtrl', function ($scope) {
+.controller('NavSideCtrl', function ($scope, $timeout) {
+
+	$scope.menu = [
+    {
+      title: 'Typography',
+      link: '/developer-guide/typography',
+      submenu: [
+      	{
+      		title: 'Headings'
+      	}
+      ]
+    },
+    {
+      title: 'Colors',
+      link: '/developer-guide/colors',
+      submenu: [
+      	{
+      		title: 'Brand'
+      	},
+      	{
+      		title: 'Pelette'
+      	}
+      ]
+    },
+    {
+      title: 'Buttons',
+      link: '/developer-guide/buttons',
+      submenu: [
+      	{
+      		title: 'Button variant'
+      	},
+      	{
+      		title: 'Disabled state'
+      	},
+      	{
+      		title: 'Active state'
+      	}
+      ]
+    }
+  ];
+
+  $timeout(function() {
+	  $('.nav--side__submenu').hide();
+	  $('.nav--side__item a').on('click', function () {
+	    var content = $(this).next('.nav--side__submenu');
+	    if(!content.is(':visible')) {
+	      content.slideDown();
+	    }
+	    else {
+	      content.slideUp();
+	    }
+	  });
+  }, 10);
 
 });
